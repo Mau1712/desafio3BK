485b49fa6f089caf0c02f460c18c5c756a20faf8
unknown <abatantuono1712@gmail.com>
2023-02-13 19:55:36 -0300

    repodesaf3

---
+++ src/app.js
@@ -0,0 +1,42 @@
+//Desafio 3. SERVIDOR con EXPRESS
+import express from 'express';
+import ProductManager from './ProductManager.js';
+
+const app = express()
+const PORT = 4000
+const manager = new ProductManager("./src/productos.json");
+
+
+app.use(express.urlencoded({extended:true})) 
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send("Desafío #3 - Servidor con Express.")
+})
+
+//Consulta de productos.
+app.get('/products', async (req,res) => {
+    const products = await manager.getProducts();
+    let {limit} = req.query;
+    let datos;
+    if(limit) {
+        datos = products.slice(0, parseInt(limit));
+        res.send(`Estos son los productos según límite: ${(JSON.stringify(datos))}`);
+    } else {
+        datos = products;
+        res.send(`Estos son todos los productos existentes: ${(JSON.stringify(datos))}`);
+    }
+    
+});
+
+//Consulta de productos según id.
+app.get('/products/:id', async (req,res) => {
+    const product = await manager.getProductById(parseInt(req.params.id));
+    product === null ? res.send("Producto inexistente") : res.send(`El producto con ID ${product.id} es el siguiente: ${(JSON.stringify(product))}`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
